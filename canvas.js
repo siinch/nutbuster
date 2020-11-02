f104e31ddaae4f3b9d22fff97ce41bed03313e4b
siinch <simon.ingemanns@protonmail.com>
2020-11-02 11:33:02 +0100

    movenut

--- canvas.js
+++ canvas.js
@@ -1,11 +1,26 @@
+console.log("Hello from canvas!")
 class Canvas {
 
+  static canvas = document.createElement("canvas");
+
   constructor () {
-    this.canvas = document.createElement("canvas");
-    this.canvas.width = 200;
-    this.canvas.height = 100;
-    this.canvas.style.border = "1px solid black";
-    this.canvas = document.body.appendChild(this.canvas);
+    Canvas.canvas.width = 800;
+    Canvas.canvas.height = 400;
+    Canvas.canvas.style.border = "5px solid gray";
+    Canvas.canvas = document.body.appendChild(Canvas.canvas);
+  }
+
+  static fill(color) {
+    let context = Canvas.canvas.getContext("2d");
+    context.beginPath();
+    context.rect(
+      0, 0,
+      Canvas.canvas.width,
+      Canvas.canvas.height);
+    context.fillStyle = color;
+    context.fill();
   }
 
 }
+
+new Canvas();
--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 console.log("Hello from main!")
-let canvas = new Canvas();
+let nut = new Nut({x: 100, y: 100}, 50, "black");
+let speed = 2.5;
+function gameloop () {
+  Canvas.fill("white");
+  nut.draw();
+  nut.position.x += speed;
+  if(nut.position.x - nut.radius < 0 || nut.position.x + nut.radius > 800)
+  speed *= -1;
+}
+setInterval(gameloop, 16);
